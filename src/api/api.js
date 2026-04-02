9ffc356f00680a690e6b5055a2f9a78f6ad46f9b
Akinnayajo Akintayo <tayoakinnayajo@gmail.com>
2026-04-02 15:00:31 +0100

    l

--- src/api/api.js
+++ src/api/api.js
@@ -1,7 +1,7 @@
 import axios from 'axios';
 
 // Base URL of your backend
-const API_BASE_URL = process.env.REACT_APP_API_URL || 'http://192.168.0.199:3000/v1';
+const API_BASE_URL = process.env.VITE_PROD_API_URL || 'https://api.lunest.app/v1';
 
 // Create axios instance
 const apiClient = axios.create({
